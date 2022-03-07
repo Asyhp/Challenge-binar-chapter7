e8f392d12de2db417cdb131947a5c5186dcda7f0
asyhp <aisyahap1595@gmail.com>
2022-03-07 21:02:14 +0700

    featinitialization

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+// const router = require('./routes')
+
+
+// const cors = require('cors')
+// const cookieParser = require('cookie-parser')
+
+
+// app.use(cors())
+// app.set('view engine', 'ejs')
+// app.set('views', 'views')
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+// app.use(express.static('public'))
+// app.use(cookieParser())
+
+// app.use(router)
+
+app.get('/', async (req, res) => {
+    res.send("hello world")
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on localhost:${PORT}`)
+})
